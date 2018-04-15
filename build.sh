0bafacc384200ff84fa05861f798bb8ce3f64bd4
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:25:24 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 sed -e '/^includesdir/ s/$(libdir).*$/$(includedir)/' -i include/Makefile.in
 sed -e '/^includedir/ s/=.*$/=@includedir@/' -e 's/^Cflags: -I${includedir}/Cflags:/' -i libffi.pc.in
 ./configure --prefix=/usr --disable-static
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+make -j $SHED_NUM_JOBS
+make DESTDIR=${SHED_FAKE_ROOT} install
