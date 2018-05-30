a5906edacdaee55cabd8ae180040255a136d86a5
Auston Stewart <auston@shedbuilt.net>
2018-05-30 09:43:20 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sed -e '/^includesdir/ s/$(libdir).*$/$(includedir)/' -i include/Makefile.in
-sed -e '/^includedir/ s/=.*$/=@includedir@/' -e 's/^Cflags: -I${includedir}/Cflags:/' -i libffi.pc.in
-./configure --prefix=/usr --disable-static
-make -j $SHED_NUM_JOBS
-make DESTDIR=${SHED_FAKE_ROOT} install
+# Patch to install headers into the standard include dir
+sed -e '/^includesdir/ s/$(libdir).*$/$(includedir)/' \
+    -i include/Makefile.in &&
+sed -e '/^includedir/ s/=.*$/=@includedir@/' \
+    -e 's/^Cflags: -I${includedir}/Cflags:/' \
+    -i libffi.pc.in &&
+# Configure
+./configure --prefix=/usr \
+            --disable-static &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
